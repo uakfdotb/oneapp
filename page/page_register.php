32f44e2fed88ee1fc55c3527cce2ea5480b6c4e2
Perennate <perennate@gmail.com>
2011-12-16 14:54:57 +0000

    Steps made in registration.

--- page/page_register.php
+++ page/page_register.php
@@ -1,4 +1,4 @@
-<h1>Register</h1>
+<h1>Registration</h1>
 
 <p>Enter the information below to register a new account. If you have already registered an account, you can <a href="login.php">login</a>. Please do not register multiple accounts under the same name: if you forgot your password, you can <a href="reset.php">reset it</a>.</p>
 
@@ -7,10 +7,10 @@
 //first display all required profile items; $profile is an array of $var_id => ($var_name, $var_desc, $var_type)
 
 foreach($profile as $var_id => $item) {
-	writeField($var_id, $item[0], $item[1], $item[2]);
+	writeField($var_id, $var_id, $item[0], $item[1], $item[2]);
 }
 ?>
 Username: <input type="text" name="username">
-<br>email: <input type="text" name="email">
+<br>Email address: <input type="text" name="email"> your password will be sent to this address
 <br><input type="submit" value="Register">
 </form>
--- register.php
+++ register.php
@@ -5,23 +5,32 @@ include("include/db_connect.php");
 include("include/session.php");
 
 if(isset($_SESSION['user_id'])) {
-	get_page("message", array("title" => "Logged In", "message" => "You are already logged in! Click <a href=\"application/\">here</a> to continue.");
-} else if(isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
-	$result = checkLogin($_REQUEST['username'], $_REQUEST['password']);
-	if($result >= 0) {
-		$_SESSION['user_id'] = $result;
-		get_page("message", array("title" => "Logged In", "message" => "You are now logged in. Click <a href=\"application/\">here</a> to continue.");
-	} else if($result == -2) {
-		get_page("message", array("title" => "Error", "message" => "Error: please try again later");
-	} else if($result == -3) {
-		get_page("message", array("title" => "Error", "message" => "Error: login and registration are currently disabled.");
-	} else if($result == -1) {
-		get_page("message", array("title" => "Error", "message" => "Error: login information is not correct.");
+	get_page("message", array("title" => "Already Logged In", "message" => "You are already logged in! Click <a href=\"application/\">here</a> to continue.");
+} else if(isset($_REQUEST['username']) && isset($_REQUEST['email'])) {
+	$data = processSubmission($_REQUEST);
+	$result = register($_REQUEST['username'], $_REQUEST['email'], $data);
+	
+	if($result == 0) {
+		get_page("message", array("title" => "Registration successful", "message" => "Your account has been created. You should be receiving an email shortly with your login details (you should change your password immediately after logging into your account) and information on how to start your application. Click <a href=\"application/\">here</a> to continue.");
+	} else if($result == 1) {
+		get_page("message", array("title" => "Error", "message" => "Error: a submitted field is too short.");
+	} else if($result == 3) {
+		get_page("message", array("title" => "Error", "message" => "Error: email address invalid or in use.");
+	} else if($result == 4) {
+		get_page("message", array("title" => "Error", "message" => "Error: internal database error!");
+	} else if($result == 5) {
+		get_page("message", array("title" => "Error", "message" => "Error: username is already in use.");
+	} else if($result == 6) {
+		get_page("message", array("title" => "Error", "message" => "Error: while sending email. Please contact us to receive your login details.");
+	} else if($result == 7) {
+		get_page("message", array("title" => "Error", "message" => "Error: you have been locked out. Try again later, or contact us.");
+	} else if($result == 8) {
+		get_page("message", array("title" => "Error", "message" => "Error: registration is currently disabled.");
 	} else {
 		get_page("message", array("title" => "Error", "message" => "Error: internal error!");
 	}
 } else {
-	get_page("login");
+	get_page("register");
 }
 
 ?>
