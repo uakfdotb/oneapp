b79e96bbd7af419f527634290a2f7cc7a94253c6
perennate@gmail.com <perennate@gmail.com@ae1f2370-0952-7bf8-395b-7c25bdf261b0>
2011-12-20 20:16:17 +0000

    Minor corrections to default pages, TODO updated.

--- trunk/include/default_pages.php
+++ trunk/include/default_pages.php
@@ -1,6 +1,6 @@
 <?php
 
-$pages['registration'] = 'Hi $USERNAME$,[br][br]Your account has been created. Your login details appear below. You can login to the application system at [url=$LOGIN_ADDRESS$]$LOGIN_ADDRESS$[/url]. You should change your password immediately after logging in.[br][br]Username: $USERNAME[br]Password: $PASSWORD$[br][br]Thanks,[br]- $site_name$';
+$pages['registration'] = 'Hi $USERNAME$,[br][br]Your account has been created. Your login details appear below. You can login to the application system at [url=$LOGIN_ADDRESS$]$LOGIN_ADDRESS$[/url]. You should change your password immediately after logging in.[br][br]Username: $USERNAME$[br]Password: $PASSWORD$[br][br]Thanks,[br]- $site_name$';
 
 $pages['index'] = '[h1]Home[/h1][p]Your application website is functional. This and other pages can be modified by logging in to the root administration system, and then selecting "manage pages".[/p]';
 
@@ -12,6 +12,6 @@ $pages['about'] = '[h1]About[/h1][p]This page has not been edited yet. This webs
 
 $pages['reset'] = 'Hi $USERNAME$,[br][br]A request has been received to reset your password. If you did not submit this request, please ignore this email; no further action needs to be taken. If you did submit this request, go to [url=$RESET_ADDRESS$]$RESET_ADDRESS$[/url] and follow the instructions there to reset your password.[br][br]Thanks,[br]- $site_name$';
 
-$pages['request_recommendation'] = 'A user has requested a recommendation from you for his or her application on $site_name$. The message appears below.[br][br]$MESSAGE$[br][br]To submit a recommendation, please go to [url=$SUBMIT_ADDRESS$]$SUBMIT_ADDRESS[/url] and follow the instructions there.[br][br]Thanks,[br]- $site_name$';
+$pages['request_recommendation'] = 'A user has requested a recommendation from you for his or her application on $site_name$. The message appears below.[br][br]$MESSAGE$[br][br]To submit a recommendation, please go to [url=$SUBMIT_ADDRESS$]$SUBMIT_ADDRESS$[/url] and follow the instructions there.[br][br]Thanks,[br]- $site_name$';
 
 ?>
