31e3851f4d5b9fcdd70c78858c823848bb6eb7d6
vaibhavtheory <vaibhavtheory@gmail.com>
2012-01-01 22:49:36 +0000

    updated layout to be in table, added message at top, but im not sure if it works or not.

--- page/page_login.php
+++ page/page_login.php
@@ -1,9 +1,24 @@
 <h1>Login</h1>
+<?
+if(isset($message)) {
+		    echo "<p>The username/password was incorrect. Please try again.</p>";
+}
+?>
 
 <p>Login here if you have activated your account. If you do not have an account, <a href="register.php">register one</a> first. If you forgot your password, you can <a href="reset.php">reset it</a>.</p>
 
-<form action="login.php" method="POST">
-Username: <input type="text" name="username">
-<br>Password: <input type="password" name="password">
-<br><input type="submit" value="Login">
+<form action="login.php" method="post">
+<table width=30%>
+<tr>
+	<td><p align="right">Username:</p></td>
+	<td><input type="text" name="username"/></td>
+</tr>
+<tr>
+	<td><p align="right">Password:</p></td>
+	<td><input type="password" name="password"/></td>
+</tr>
+<tr>
+	<td colspan="2" align="right"><input type="submit" value="Login" align="center"/></td>
+</tr>
+</table>
 </form>
