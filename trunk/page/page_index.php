3f4c3d54645d88a256ad115137f5879201959ee8
vaibhavtheory@gmail.com <vaibhavtheory@gmail.com@ae1f2370-0952-7bf8-395b-7c25bdf261b0>
2012-01-01 06:33:39 +0000

    did css basic

--- trunk/page/page_index.php
+++ trunk/page/page_index.php
@@ -1,1 +1,63 @@
-<?= page_db("index"); ?>
+<div id="imgmain" >
+			<img src="images/logo.jpg" alt="logo" width="680"/>
+			     </div>
+				<div id="schoolname">
+					<h4>SCHOOL NAME<p>Not your school? <a href="#">Click Here</a></p></h4>
+						   </div>
+							
+								<div class="spacer"></div>
+								     
+									<div id="logbox">
+									     <div id="col_left">
+											<div class="borderonlow">
+												<table cellpadding="0" cellspacing="0" width=100% height=80%>
+												       		       		       <tr bgcolor=#1A3E5B><td class="center"><h2>Javascript #1</h2></td></tr>
+																       	   		       				     <tr style="height:100%;" bgcolor=#F2F5F7><td>
+																							     	 		       </td>
+																										          </tr>
+																											    </table>
+																											      </div>
+																											       </div>
+																											        <div id="col_mid" class="borderon">
+																												     		  <div class="center">
+																														        <table cellpadding="0" cellspacing="0" width=100% height=100%>
+																															       		      		       <tr bgcolor=#1A3E5B><td class="center"><h2>Log In</h2></td></tr>
+																																			       	   		       			      <tr bgcolor=#F2F5F7><td>
+																																									      	     <form action="login.php" method="POST">
+																																										     	      <table width=100%>
+																																											      	         <tr>
+																																													       <td><p align="right">Username:</p></td>
+																																													       	           <td><input type="text" name="username" size=6 /></td>
+																																															   	      		  	       	       </tr>
+																																																				              <tr>
+																																																					            <td><p align="right">Password:</p></td>
+																																																						    	        <td><input type="text" name="password" size=6 /></td>
+																																																									   	       		    	    </tr>
+																																																													           <tr>
+																																																														           <td colspan="2" align="right"><input type="submit" value="Log In" align="center"/></td>
+																																																															       		   				        	 </tr>
+																																																																							       </table>
+																																																																							           </form>
+
+																																																																								      </td>
+																																																																								         </tr>
+																																																																									    <tr bgcolor=#DEDEDE><td>
+																																																																									    	   <p class="test">Not Registered? <a href="#">Click Here</a></p>
+																																																																										      		       		      <p class="test">Forgot Password? <a href="#">Click Here</a></p>
+																																																																														      	 		     	       	  <p class="test">Forgot Username? <a href="#">Click Here</a></p><p></p>
+																																																																																			     			 	   </td>
+																																																																																							      </tr>
+																																																																																							        </table>
+																																																																																								  </div>
+																																																																																								   </div>
+																																																																																								    <div id="col_right">
+																																																																																								    	  <div class="borderonlow">
+																																																																																									        <table cellpadding="0" cellspacing="0" width=100% height=80%>
+																																																																																										       		       		       <tr bgcolor=#1A3E5B><td class="center"><h2>Javascript #2</h2></td></tr>
+																																																																																														       	   		       				     <tr style="height:100%;" bgcolor=#F2F5F7><td>
+																																																																																																					     	 		       </td>
+																																																																																																								          </tr>
+																																																																																																									    </table>
+																																																																																																									      </div>
+																																																																																																									       </div>
+																																																																																																									        <br />
