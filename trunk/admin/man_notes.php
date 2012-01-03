0d743cb659bac8f3e4afc3289290fa505d09ff1b
perennate@gmail.com <perennate@gmail.com@ae1f2370-0952-7bf8-395b-7c25bdf261b0>
2012-01-03 01:28:48 +0000

    Added notes features (textbox and categories).

--- trunk/admin/man_notes.php
+++ trunk/admin/man_notes.php
@@ -10,28 +10,59 @@ if(isset($_SESSION['admin_id'])) {
 	$club_id = escape(getAdminClub($_SESSION['admin_id']));
 	
 	if($club_id != 0) {
-		if(isset($_REQUEST['cat_enabled']) && isset(['box_enabled'])) {
-			$cat_enabled = 
+		//first, notes enabled/disabled settings
+		if(isset($_REQUEST['notesupdate'])) {
+			$box_enabled = isset($_REQUEST['box_enabled']) ? 1 : 0;
+			$cat_enabled = isset($_REQUEST['cat_enabled']) ? 1 : 0;
 			
-			mysql_query("UPDATE clubs SET description='$description', view_time='$view_time', open_time='$open_time', close_time='$close_time', num_recommend='$num_recommend' WHERE id='$club_id'");
+			mysql_query("UPDATE admins SET box_enabled = '$box_enabled', cat_enabled = '$cat_enabled' WHERE id = '" . $_SESSION['admin_id'] . "'");
 		}
 		
-		$result = mysql_query("SELECT description, view_time, open_time, close_time, num_recommend FROM clubs WHERE id='$club_id'");
+		$result = mysql_query("SELECT box_enabled, cat_enabled FROM admins WHERE id='" . $_SESSION['admin_id'] . "'");
 		
 		if($row = mysql_fetch_array($result)) {
+			$box_checked = "";
+			$cat_checked = "";
+			
+			if($row['box_enabled'] == 1) $box_checked = " checked";
+			if($row['cat_enabled'] == 1) $cat_checked = " checked";
 ?>
-			<form method="post" action="man_club.php">
-			Description<br> <textarea name="description"><?= $row['description'] ?></textarea><br>
-			View time <input type="text" name="view_time" value="<?= date('m/d/y H:i:s', $row['view_time']) ?>" /> MM/DD/YY (hh:mm:ss)<br>
-			Open time <input type="text" name="open_time" value="<?= date('m/d/y H:i:s', $row['open_time']) ?>" /><br>
-			Close time <input type="text" name="close_time" value="<?= date('m/d/y H:i:s', $row['close_time']) ?>" /><br>
-			Number of recommenations <input type="text" name="num_recommend" value="<?= $row['num_recommend'] ?>" /><br>
-			<input type="submit" value="Update" />
+			<form method="post" action="man_notes.php">
+			<input type="checkbox" name="box_enabled" value="true" <?= $box_checked ?>/> Textboxes enabled
+			<br /><input type="checkbox" name="cat_enabled" value="true" <?= $cat_checked ?>/> Categories enabled
+			<br /><input type="submit" name="notesupdate" value="Update" />
 			</form>
 <?
-		} else {
-			echo "Error: your club cannot be found in the clubs table.<br>";
 		}
+		
+		//now, the categories for the notes_category feature
+		
+		if(isset($_REQUEST['name']) && isset($_REQUEST['action'])) {
+			$catName = escape($_REQUEST['name']);
+			
+			if($_REQUEST['action'] == "delete") {
+				mysql_query("DELETE FROM club_notes_categories WHERE club_id = '$club_id' AND name = '$catName'");
+			} else if($_REQUEST['action'] == "add") {
+				mysql_query("INSERT INTO club_notes_categories (name, club_id) VALUES ('$catName', '$club_id')");
+			}
+		}
+		
+		$result = mysql_query("SELECT name FROM club_notes_categories WHERE club_id = '$club_id'");
+		echo "<table><tr><th>Category name</th><th>Delete</th></tr>";
+		
+		while($row = mysql_fetch_array($result)) {
+			echo "<tr><td>" . $row[0] . "</td>";
+			echo "<td><form method=\"post\" action=\"man_notes.php?name=" . urlencode($row[0]) . "\">";
+			echo "<input type=\"submit\" name=\"action\" value=\"delete\" />";
+			echo "</form></td></tr>";
+		}
+		
+		echo "</table>";
+		
+		echo '<form method="POST" action="man_notes.php?action=add">';
+		echo 'Category name <input type="text" name="name" />';
+		echo '<input type="submit" value="Add category" />';
+		echo '</form>';
 	} else {
 		echo "General application cannot view submissions, so note functions are not available.<br>";
 	}
--- trunk/admin/view_submit.php
+++ trunk/admin/view_submit.php
@@ -14,37 +14,94 @@ if(isset($_SESSION['admin_id'])) {
 	//check if this admin is using textboxes and categories
 	$result = mysql_query("SELECT box_enabled, cat_enabled FROM admins WHERE id='" . escape($_SESSION['admin_id']) . "'");
 	$row = mysql_fetch_array($result);
-	$box_enabled = $row['box_enabled'];
-	$cat_enabled = $row['cat_enabled'];
-	
-	$array = listCompletedApplications($club_id);
-
-	echo "<table><tr><th>App ID</th><th>User ID</th><th>General application</th><th>Supplement</th><th>Peer recommendations</th>";
-	
-	if($box_enabled) echo "<th>The Box</th>";
-	if($cat_enabled) {
-		echo "<th>Category</th>";
+	$box_enabled = ($row['box_enabled'] == 1) ? true : false;
+	$cat_enabled = ($row['cat_enabled'] == 1) ? true : false;
 		
-		//we also need to retrieve the list of categories
-		$catResult = mysql_query("SELECT name FROM club_notes_categories WHERE club_id = '$club_id'");
-		$catList = array();
-		
-		while($row = mysql_fetch_array($catResult)) {
-			array_push($catList, $row[0]);
-		}
-	}
-	
+	//we will need to construct a map from the application database IDs to the box and category note values if enabled
+	// we do this now so that we can check if an entry exists already for updating the notes table
 	if($box_enabled || $cat_enabled) {
-		echo "<th>Update</th>";
-		
-		//we also need to construct a map from the application database IDs to the box and category note values
 		$toolsResult = mysql_query("SELECT application_id, box, category FROM club_notes WHERE club_id = '$club_id'");
 		$toolsMap = array();
-		
+	
 		while($row = mysql_fetch_array($toolsResult)) {
 			$toolsMap[$row['application_id']] = array($row['box'], $row['category']);
 		}
 	}
+	
+	//check if notes table needs to be updated; use toolsMap to see if INSERT or UPDATE should be used
+	if(($box_enabled || $cat_enabled) && isset($_REQUEST['id'])) {
+		$application_id = escape($_REQUEST['id']);
+		
+		//verify that it belongs to this admin's club
+		$result = mysql_query("SELECT club_id FROM applications WHERE id = '$application_id'");
+		
+		if($row = mysql_fetch_array($result)) { //application exists
+			if($row[0] == $club_id) { //club is correct
+				//if the tools map does not contain the ID, then we have to add it to database
+				if(!isset($toolsMap[$application_id])) {
+					$toolsMap[$application_id] = array('', '');
+					mysql_query("INSERT INTO club_notes (application_id, club_id) VALUES ('$application_id', '$club_id')");
+				}
+				
+				//now we check what needs to be updated
+				// since we pre-retrieved toolsMap, we have to update it to reflect the changes
+				
+				$updateString = "";
+		
+				if($box_enabled && isset($_REQUEST['box'])) {
+					$updateString .= "box = '" . escape($_REQUEST['box']) . "', ";
+					$toolsMap[$application_id][0] = $_REQUEST['box'];
+				}
+		
+				if($cat_enabled && isset($_REQUEST['category'])) {
+					$updateString .= "category = '" . escape($_REQUEST['category']) . "', ";
+					$toolsMap[$application_id][1] = $_REQUEST['category'];
+				}
+			
+				if(strlen($updateString) > 0) {
+				
+					$updateString = substr($updateString, 0, -2);
+					mysql_query("UPDATE club_notes SET $updateString WHERE application_id='$application_id' AND club_id='$club_id'");
+				}
+			}
+		}
+	}
+	
+	//box filter manager currently nonexistent
+	
+	//category filter manager
+	// first, we retrieve a list of categories (this will be used in dropdown as well)
+	if($cat_enabled) {
+		$catResult = mysql_query("SELECT name FROM club_notes_categories WHERE club_id = '$club_id'");
+		$catList = array();
+	
+		while($row = mysql_fetch_array($catResult)) {
+			array_push($catList, $row[0]);
+		}
+	
+		// filter might already be set, so retrieve it
+		$catFilter = "";
+		if(isset($_REQUEST['catFilter'])) $catFilter = $_REQUEST['catFilter'];
+	
+		// now, we give user a filter selection dropdown, preselecting the current filter if any
+		echo "Category filter: <select name=\"catFilter\">";
+	
+		foreach($catList as $catElement) {
+			$selectedString = ($catElement == $catFilter) ? " selected" : "";
+			echo "<option name=\"$catElement\"$selectedString>$catElement</option>";
+		}
+	
+		echo "</select><br />";
+	}
+	
+	//list the applications, applying filters only when we loop (inefficient but okay)
+	$array = listCompletedApplications($club_id);
+
+	echo "<table><tr><th>App ID</th><th>User ID</th><th>General application</th><th>Supplement</th><th>Recommendations</th>";
+	
+	if($box_enabled) echo "<th>The Box</th>";
+	if($cat_enabled) echo "<th>Category</th>";
+	if($box_enabled || $cat_enabled) echo "<th>Update</th>";
 	
 	echo "</tr>";
 
@@ -53,12 +110,12 @@ if(isset($_SESSION['admin_id'])) {
 		echo "<form method=\"post\" action=view_submit.php?id=$appId>";
 		
 		$userId = '<a href="user_detail.php?id=' . $item[1] . '">' . $item[1] . '</a>';
-		$generalApp = '<a href="../submit/' . $item[2] . '.pdf">' . $item[2] . '</a>';
-		$supplement = '<a href="../submit/' . $item[3] . '.pdf">' . $item[3] . '</a>';
+		$generalApp = '<a href="../submit/' . $item[2] . '.pdf">link</a>';
+		$supplement = '<a href="../submit/' . $item[3] . '.pdf">link</a>';
 		
 		$peerString = "";
 		foreach($item[4] as $peerEntry) {
-			$peerString .= '<a href="../submit/' . $peerEntry . '.pdf">' . $peerEntry . '</a> | ';
+			$peerString .= '<a href="../submit/' . $peerEntry . '.pdf">link</a> | ';
 		}
 	
 		echo "<tr><td>$appId</td><td>$userId</td><td>$generalApp</td><td>$supplement</td><td>$peerString</td>";
@@ -82,7 +139,7 @@ if(isset($_SESSION['admin_id'])) {
 				echo "<option name=\"$catElement\"$selectedString>$catElement</option>";
 			}
 			
-			echo "</td>";
+			echo "</select></td>";
 		}
 		
 		if($box_enabled || $cat_enabled) {
