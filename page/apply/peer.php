5a0d6740d3c98a5b011296c9b732d68f7c3c55e2
controlpc48 <controlpc48@gmail.com>
2012-01-05 23:06:43 +0000

    peer layout updated

--- page/apply/peer.php
+++ page/apply/peer.php
@@ -8,16 +8,16 @@ if(isset($message)) {
 
 <p>The recommendations that you have already requested are listed below. For information on submitting different peer recommendations to different clubs, click <a target="_blank" href="../help.php?page=submit_different">here</a>.</p>
 
-<table><tr><th>Name</th><th>Email</th><th>Status</th><th>Toggle (<a target="_blank" href="../help.php?page=submit_different">?</a>)</th></tr>
+<table width=100% class="borderon"><tr align="center"><th><p class="admin_table_header">Name</p></th><th><p class="admin_table_header">Email</p></th><th><p class="admin_table_header">Status</p></th><th><p class="admin_table_header">Toggle (<a target="_blank" href="../help.php?page=submit_different">?</a>)</th></tr>
 
 <?
 foreach($recList as $rec) {
-	echo "<tr><td>" . $rec[1] . "</td>";
-	echo "<td>" . $rec[2] . "</td>";
+	echo "<tr align=\"center\"><td><p>" . $rec[1] . "</p></td>";
+	echo "<td><p>" . $rec[2] . "</p></td>";
 	
 	$statusString = ($rec[3] == "0") ? "incomplete" : "complete (enabled)";
 	if($rec[3] == 2) $statusString = "complete (disabled)";
-	echo "<td>" . $statusString . "</td>";
+	echo "<td><p>" . $statusString . "<p></td>";
 	
 	echo "<td><form method=\"POST\" action=\"peer.php?toggle=true&id=" . $rec[0] . "\">";
 	echo "<input type=\"submit\" value=\"Toggle\">";
@@ -29,11 +29,12 @@ foreach($recList as $rec) {
 
 <p>Use the form below to request a peer recommendation.</p>
 
-<form action="peer.php" method="POST">
-Name: <input type="text" name="name"><br>
-Email address: <input type="text" name="email"><br>
-Message:<br><textarea rows="6" cols="50" name="message">Dear ___,
+<form action="peer.php" method="POST"><table>
+<tr><td><p class="name">*Name:</p></td><td><input type="text" name="name" style="width:100%"></td></tr>
+<tr><td><p class="name">*Email address:</p></td><td><input type="text" name="email" style="width:100%"></td></tr>
+<tr><td colspan="2"><p class="name">*Message:</p><textarea rows="6" cols="50" name="message" style="width:100%;resize:none">Dear [insert name here],
 
-This is ___ ___. I would like to request a peer recommendation from you. I have already communicated with you about this; the details for submitting the recommendation should appear above.</textarea>
-<input type="submit" value="Request">
+This is [Insert Your Name]. I would like to request a peer recommendation from you. I have already communicated with you about this; the details for submitting the recommendation should appear above.</textarea></td></tr>
+<tr><td colspan="2" align="right"><input type="submit" value="Request"></td></tr>
+</table>
 </form>
