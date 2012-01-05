14b9d54e3ee0e8f59130d471d5a877bca77e84db
Perennate <perennate@gmail.com>
2012-01-05 17:17:00 +0000

    Added state handling to application/clubDetail, so that user can see if their application can be submitted, has been submitted, or is not in submittable window.

--- application/clubDetail.php
+++ application/clubDetail.php
@@ -4,13 +4,22 @@ include("../config.php");
 include("../include/db_connect.php");
 include("../include/session.php");
 
+include("../include/apply_gen.php");
+
 if(isset($_SESSION['user_id'])) {
 	if(isset($_REQUEST['app_id']) && isset($_REQUEST['club_id'])) {
 		$club_id = $_REQUEST['club_id'];
 		$app_id = $_REQUEST['app_id'];
 		
 		$clubInfo = clubInfo($club_id);
-		get_page_advanced("club_detail", "apply", array("club_id" => $club_id, "app_id" => $app_id, "clubInfo" => $clubInfo));
+		//passed to page, and also used to validate ownership of application
+		$checkStatus = checkApplication($_SESSION['user_id'], $app_id);
+		
+		if($checkStatus != -2) {
+			get_page_advanced("club_detail", "apply", array("club_id" => $club_id, "app_id" => $app_id, "clubInfo" => $clubInfo, 'checkStatus' => $checkStatus));
+		} else {
+			get_page_advanced("message", "apply", array("title" => "Internal error", "message" => "Internal error: application does not appear to belong to the requesting user."));
+		}
 	} else {
 		get_page_advanced("message", "apply", array("title" => "Internal error", "message" => "Internal error: app_id or club_id unspecified."));
 	}
