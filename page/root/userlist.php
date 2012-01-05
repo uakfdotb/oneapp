4bf8969f437df0f0140b6438e7e0b4e3a4934f49
controlpc48 <controlpc48@gmail.com>
2012-01-05 23:19:59 +0000

    band colors, layout

--- page/root/userlist.php
+++ page/root/userlist.php
@@ -2,7 +2,7 @@
 
 <p>Here, all registered users are listed. Note that this includes users who have registered but not logged in yet (highlighted). <b>Reset apps</b> will delete all applications and responses of the user from the database, including on the general application. <b>Delete user</b> will delete the user completely (but will not delete applications and responses).</p>
 
-<table width=100% class="borderon">
+<table width=100% class="borderon" cellspacing="0">
 <tr>
 	<th><p class="admin_table_header">ID</p></th>
 	<th><p class="admin_table_header">Username</p></th>
@@ -23,11 +23,11 @@ foreach($userList as $user_id => $user) {
 	$profileUser = $user[2]; //contains profile fields
 	
 	$banding = "";
-	if($user[0] == 0) $banding = " id=\"bandwarning\"";
+	if($user[0] == 0) $banding = " class=\"bandwarning\"";
 ?>
 	
 	<form method="post" action="userlist.php">
-	<input type="hidden" name="id" value="<?= $user_id ?>"><tr align="center"<?= $banding ?>>
+	<input type="hidden" name="id" value="<?= $user_id ?>"><tr align="center"<?= $banding ?> style="padding-top:1">
 	<td><p><?= $user_id ?></p></td>
 	<td><p><?= $infoUser[0] ?></p></td>
 	<td><p><?= $infoUser[1] ?></p></td>
