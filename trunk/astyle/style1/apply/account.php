4c3600f9542807c16aceb534482fd498aebeea5b
perennate@gmail.com <perennate@gmail.com@ae1f2370-0952-7bf8-395b-7c25bdf261b0>
2012-01-05 17:07:05 +0000

    Updated documentation/TODO, and added unactivated user feature.

---
+++ trunk/astyle/style1/apply/account.php
@@ -0,0 +1,38 @@
+<h1>Account</h1>
+
+<?
+if(isset($message)) {
+	echo "<p>" . $message . "</p>";
+}
+?>
+
+<table>
+<form action="account.php" method="POST">
+<tr>
+	<td><p>Username</p></td>
+	<td><p><?= $userInfo[0] ?></p></td>
+</tr><tr>
+	<td><p>Old password</p></td>
+	<td><p><input type="password" name="old_password"> enter old password here to update information</p></td>
+</tr>
+<?
+//profile information
+foreach($profile as $item) {
+	echo "<tr><td><p>" . $item[0] . "</p></td><td><p>" . $item[1] . "</p></td></tr>";
+}
+?>
+
+<tr>
+	<td><p>New password</p></td>
+	<td><p><input type="password" name="new_password"> to change your password, fill in this and the below field</p></td>
+</tr><tr>
+	<td><p>Confirm new password</p></td>
+	<td><p><input type="password" name="new_password_conf"></p></td>
+</tr><tr>
+	<td><p>Email</p></td>
+	<td><p><input type="text" name="new_email" value="<?= $userInfo[1] ?>"></p></td>
+</tr><tr>
+	<td></td>
+	<td><input type="submit" value="Update"></td>
+</table>
+</form>
--- trunk/page/apply/account.php
+++ trunk/page/apply/account.php
@@ -6,17 +6,33 @@ if(isset($message)) {
 }
 ?>
 
+<table>
 <form action="account.php" method="POST">
-Username: <?= $userInfo[0] ?>
-<br>Old password: <input type="password" name="old_password"> enter old password here to update information
+<tr>
+	<td>Username</td>
+	<td><?= $userInfo[0] ?></td>
+</tr><tr>
+	<td>Old password</td>
+	<td><input type="password" name="old_password"> enter old password here to update information</td>
+</tr>
 <?
 //profile information
 foreach($profile as $item) {
-	echo "<br>" . $item[0] . ": " . $item[1];
+	echo "<tr><td>" . $item[0] . "</td><td>" . $item[1] . "</td></tr>";
 }
 ?>
-<br>New password: <input type="password" name="new_password"> to change your password, fill in this and the below field
-<br>Confirm new password: <input type="password" name="new_password_conf">
-<br>Email: <input type="text" name="new_email" value="<?= $userInfo[1] ?>">
-<br><input type="submit" value="Update">
+
+<tr>
+	<td>New password</td>
+	<td><input type="password" name="new_password"> to change your password, fill in this and the below field</td>
+</tr><tr>
+	<td>Confirm new password</td>
+	<td><input type="password" name="new_password_conf"></td>
+</tr><tr>
+	<td>Email</td>
+	<td><input type="text" name="new_email" value="<?= $userInfo[1] ?>"></td>
+</tr><tr>
+	<td></td>
+	<td><input type="submit" value="Update"></td>
+</table>
 </form>
