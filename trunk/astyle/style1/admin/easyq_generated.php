e6e061ba98f555f0c602e84db5ecc5822310a649
perennate@gmail.com <perennate@gmail.com@ae1f2370-0952-7bf8-395b-7c25bdf261b0>
2012-01-06 05:34:05 +0000

    Modified easyq_generated to show textbox for style1.

---
+++ trunk/astyle/style1/admin/easyq_generated.php
@@ -0,0 +1,5 @@
+<h1>Easy Question Adder</h1>
+
+<p>The three-line format has been generated and should appear below. You may wish to <a href="man_questions.php">add your question</a> or <a href="easy_question.php">generate another one</a>.</p>
+
+<textarea cols="60" rows="10"><?= $generate ?></textarea>
