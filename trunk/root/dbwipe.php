7ddf84e60166598e6adb28e0157ef5fc4a401c18
perennate@gmail.com <perennate@gmail.com@ae1f2370-0952-7bf8-395b-7c25bdf261b0>
2012-01-06 18:50:37 +0000

    Added database wipe functionality.

---
+++ trunk/root/dbwipe.php
@@ -0,0 +1,26 @@
+<?php
+include("../config.php");
+include("../include/common.php");
+include("../include/db_connect.php");
+include("../include/session.php");
+
+include("../include/chk.php");
+
+if(isset($_SESSION['root'])) {
+	$message = "";
+	
+	if(isset($_REQUEST['wipe']) && isset($_REQUEST['password'])) {
+		if(checkRoot($_REQUEST['password'])) {
+			databaseWipe();
+			$message = "Your database has been completely wiped.";
+		} else {
+			$message = "Invalid password. You have been logged out.";
+			session_unset();
+		}
+	}
+	
+	get_page_advanced("dbwipe", "root", array('message' => $message));
+} else {
+	header('Location: index.php');
+}
+?>
