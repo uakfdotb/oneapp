55f7fa2ad4b68360f2b5db44c20679039879ea4b
Perennate <perennate@gmail.com>
2012-01-06 04:08:34 +0000

    Edited statistics to actually say users who submitted.

--- page/admin/easyq_type.php
+++ page/admin/easyq_type.php
@@ -1,3 +1,6 @@
 <h1>Easy Question Adder</h1>
 
-<p>This tool generates three-line question (todo...)</p>
+<p>This tool generates questions in the three-line format. You can then copy and paste the generated content into the multi-add form in your question manager. First, select a type from the list.</p>
+
+<ul>
+<li><a href="easy_question">
