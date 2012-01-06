295213fc5e0746df34035848c474dbb68e7d5682
controlpc48@gmail.com <controlpc48@gmail.com@ae1f2370-0952-7bf8-395b-7c25bdf261b0>
2012-01-06 02:45:53 +0000

    userlist done

--- trunk/page/root/userlist.php
+++ trunk/page/root/userlist.php
@@ -2,7 +2,7 @@
 
 <p>Here, all registered users are listed. Note that this includes users who have registered but not logged in yet (highlighted). <b>Reset apps</b> will delete all applications and responses of the user from the database, including on the general application. <b>Delete user</b> will delete the user completely (but will not delete applications and responses).</p>
 
-<table width=100% class="borderon" cellspacing="0">
+<table width=100% cellspacing="0">
 <tr>
 	<th><p class="admin_table_header">ID</p></th>
 	<th><p class="admin_table_header">Username</p></th>
@@ -21,14 +21,14 @@ foreach($userList as $user_id => $user) {
 ?>
 	
 	<form method="post" action="userlist.php">
-	<input type="hidden" name="id" value="<?= $user_id ?>"><tr align="center"<?= $banding ?> style="padding-top:1">
-	<td><p><a href="user_detail.php?id=<?= $user_id ?>"><?= $user_id ?></a></p></td>
-	<td><p><?= $infoUser[0] ?></p></td>
-	<td><p><?= $infoUser[1] ?></p></td>
-	<td><p><?= timeString($user[0]) ?></p></td>
-	<td><input type="submit" name="action" value="reset"></td>
-	<td><input type="submit" name="action" value="delete!!"></td>
-	</form></tr>
+	<input type="hidden" name="id" value="<?= $user_id ?>"><tr align="center"<?= $banding ?> style="padding-top:1"><a href="user_detail.php?id=<?= $user_id ?>">
+	<td class="top_border"><p><a href="user_detail.php?id=<?= $user_id ?>"><?= $user_id ?></a></p></td>
+	<td class="top_border"><p><a href="user_detail.php?id=<?= $user_id ?>"><?= $infoUser[0] ?></a></p></td></a>
+	<td class="top_border"><p><?= $infoUser[1] ?></p></td>
+	<td class="top_border"><p><?= timeString($user[0]) ?></p></td>
+	<td class="top_border"><input type="submit" name="action" value="reset"></td>
+	<td class="top_border"><input type="submit" name="action" value="delete!!"></td>
+	</tr></form>
 <? } ?>
 
 </table>
