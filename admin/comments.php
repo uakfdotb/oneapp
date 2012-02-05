819f17a2a19e4e11ba26666d52b40734ff8ef27c
Perennate <perennate@gmail.com>
2012-02-05 23:22:16 +0000

    Fixed comments box.

--- admin/comments.php
+++ admin/comments.php
@@ -7,12 +7,13 @@ include("../include/session.php");
 //check for $_REQUEST['id'] here because this page should never be accessed without an id set
 if(isset($_SESSION['admin_id']) && $_REQUEST['id']) {
 	$club_id = escape(getAdminClub($_SESSION['admin_id']));
+	$admin_id = escape($_SESSION['admin_id']);
 	$app_id = escape($_REQUEST['id']);
 	
 	if($club_id != 0) {
 		//retrieve and display comments (if not found in table, comments have not been set yet so default to blank)
 		$current_comments = "";
-		$result = mysql_query("SELECT comments FROM club_notes WHERE application_id='$app_id' AND club_id='$club_id'");
+		$result = mysql_query("SELECT comments FROM club_notes WHERE application_id='$app_id' AND admin_id='$admin_id'");
 		if($row = mysql_fetch_array($result)) {
 			$current_comments = $row[0];
 		}
