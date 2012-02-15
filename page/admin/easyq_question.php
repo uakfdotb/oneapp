cd0950a2a9a13267684fd415402b3c1c9c0e5024
controlpc48 <controlpc48@gmail.com>
2012-02-15 09:57:47 +0000

    latex file was not updated before this, root side display now has sub menus, easyq now matches TAMSapp

--- page/admin/easyq_question.php
+++ page/admin/easyq_question.php
@@ -1,11 +1,15 @@
 <h1>Easy Question Adder</h1>
 
 <p>Select from the following options.</p>
+<? if($type == "essay" || $type == "short" || $type == "text") { ?>
+	<p>Do not leave Question or Descrition blank. In the case you do not want anything, add a space.</p>
+	</p>
+<? } ?>
 
 <form method="POST" action="easy_question.php">
 <input type="hidden" name="type" value="<?= $type ?>" />
 
-<p>Question name: <input type="text" name="name" /> (not shown for text questions)</p>
+<p>Question <br /><textarea name="name" /></textarea></p>
 
 <!-- description; required field -->
 <? if($type == "select") { ?>
@@ -37,14 +41,20 @@ if($type == "essay" || $type == "short") {
 	else $length = 80;
 ?>
 	<p>
-	<input type="text" name="length" value="<?= $length ?>" /> Maximum number of characters allowed
+	<input type="text" name="length" value="<?= $length ?>" /> Maximum number of characters allowed.
 	</p>
+	<table width="60%" class="borderon"><tr align="center"><td width="30%"><p><b>Characters</b></p></td><td><p><b>Approximate Equivialant</b></p></td></tr>
+	<tr align="center"><td><p>80</p></td><td><p>1 sentance</p></td></tr>
+	<tr align="center"><td><p>450</p></td><td><p>1 paragraph</p></td></tr>
+	<tr align="center"><td><p>2500</p></td><td><p>450 words</p></td></tr>	
+	<tr align="center"><td><p>6000</p></td><td><p>1000 words</p></td></tr>
+	</table>
 <? } ?>
 
 <!-- method:single,multiple,dropdown; default is single -->
 <? if($type == "select") { ?>
 	<p>Selection method<br />
-	<input type="radio" name="method" value="single" /> Single selection (radio button, like this)<br />
+	<input type="radio" name="method" value="single" checked /> Single selection (radio button, like this)<br />
 	<input type="radio" name="method" value="multiple" /> Multiple selection (checkboxes)<br />
 	<input type="radio" name="method" value="dropdown" /> Dropdown selection
 	</p>
@@ -53,7 +63,7 @@ if($type == "essay" || $type == "short") {
 <!-- size:medium,large,huge; default is medium -->
 <? if($type == "essay") { ?>
 	<p>Essay size, determines size of text area<br />
-	<input type="radio" name="size" value="medium" /> Medium<br />
+	<input type="radio" name="size" value="medium" checked/> Medium *SUGGESTED*<br />
 	<input type="radio" name="size" value="large" /> Large<br />
 	<input type="radio" name="size" value="huge" /> Huge
 	</p>
