a8bd9f439b3889ed9d99a1deeb7807466437cc6a
Perennate <perennate@gmail.com>
2012-02-25 21:31:36 +0000

    Fixed the calendar on club manager.

--- astyle/one/admin/man_club.php
+++ astyle/one/admin/man_club.php
@@ -10,10 +10,10 @@
 
 <form method="post" action="man_club.php">
 <table><tr>
-<td colspan="2"><p>Description</p></td><tr><td colspan="2"><textarea name="description"><?= $description ?></textarea></td></tr>
-<tr><td><p>View time</p></td><td><p>MM/DD/YY (hh:mm:ss)<br><input type="text" name="view_time" value="<?= date('m/d/y H:i:s', $view_time) ?>" /><img src="<?= $stylePath ?>/datetimepicker/images/cal.gif" onclick="javascript:NewCssCal('view_time', 'MMddyyyy', 'arrow', true)" style="cursor:pointer"/></p></td></tr>
-<tr><td><p>Open time</p></td><td><input type="text" name="open_time" value="<?= date('m/d/y H:i:s', $open_time) ?>" /><img src="<?= $stylePath ?>/datetimepicker/images/cal.gif" onclick="javascript:NewCssCal('open_time', 'MMddyyyy', 'arrow', true)" style="cursor:pointer"/></td></tr>
-<tr><td><p>Close time</p></td><td><input type="text" name="close_time" value="<?= date('m/d/y H:i:s', $close_time) ?>" /><img src="<?= $stylePath ?>/datetimepicker/images/cal.gif" onclick="javascript:NewCssCal('close_time', 'MMddyyyy', 'arrow', true)" style="cursor:pointer"/></td></tr>
+<td colspan="2"><p>Description</p></td><tr><td colspan="2"><textarea width="100%" rows="8" name="description"><?= $description ?></textarea></td></tr>
+<tr><td><p>View time</p></td><td><p>MM/DD/YY (hh:mm:ss)<br><input type="text" id="view_time" name="view_time" value="<?= date('m/d/y H:i:s', $view_time) ?>" /><img src="<?= $stylePath ?>/datetimepicker/images/cal.gif" onclick="javascript:NewCssCal('view_time', 'MMddyyyy', 'arrow', true)" style="cursor:pointer"/></p></td></tr>
+<tr><td><p>Open time</p></td><td><input type="text" id="open_time" name="open_time" value="<?= date('m/d/y H:i:s', $open_time) ?>" /><img src="<?= $stylePath ?>/datetimepicker/images/cal.gif" onclick="javascript:NewCssCal('open_time', 'MMddyyyy', 'arrow', true)" style="cursor:pointer"/></td></tr>
+<tr><td><p>Close time</p></td><td><input type="text" id="close_time" name="close_time" value="<?= date('m/d/y H:i:s', $close_time) ?>" /><img src="<?= $stylePath ?>/datetimepicker/images/cal.gif" onclick="javascript:NewCssCal('close_time', 'MMddyyyy', 'arrow', true)" style="cursor:pointer"/></td></tr>
 <tr><td><p>Number of recommenations</p></td><td><input type="text" name="num_recommend" value="<?= $num_recommend ?>" /></td></tr>
 <tr><td><p>Update Password</p></td><td><input type="password" name="new_password"></td></tr>
 <tr><td colspan ="2" align="right"><input type="submit" value="Update" /></td></tr>
