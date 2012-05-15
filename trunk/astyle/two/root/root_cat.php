db865838f64cacc92edb74ecb55d597e942832d9
controlpc48@gmail.com <controlpc48@gmail.com@ae1f2370-0952-7bf8-395b-7c25bdf261b0>
2012-05-15 22:31:56 +0000

    deleted control pages, added same layout to root_cat pages

--- trunk/astyle/two/root/root_cat.php
+++ trunk/astyle/two/root/root_cat.php
@@ -2,13 +2,24 @@
 
 <p><?= $config['root_cat_display'][$cat]['desc'] ?></p>
 
-<ul>
 <?
 	$root_display = $config['root_cat_display'][$cat]['links'];
 	$root_display_names = $config['root_cat_display'][$cat]['names']; 
 	
-	for($j = 0; $j < count($root_display); $j++) {
-		echo '<li><a href=' . $root_display[$j] . '.php>' . $root_display_names[$j] . '</a></li>';	
-	}
 ?>
-</ul>
+
+<table class="nav-container">			
+			<tr><td>
+<?	for($j = 0; $j < count($root_display); $j++) { ?>
+				<a href="<?=$root_display[$j] ?>.php"><div class="nav-button <? if(($j %2 == 0 && $j %4 ==0) || ($j %2==1 && ($j+1) %4==0)) echo 'alternate'; ?> ">
+					<img src="<?=$stylePath?>/images/root/<?=$root_display[$j] ?>.png" height="56" style="margin-left:-3px">
+					<h2><?=$root_display_names[$j]?></h2>
+				</div></a>
+			<? if($j %2 == 0) {
+					echo "</td><td>";
+			   } else {
+			   		echo "</td></tr><tr><td>";
+			   } 
+	} ?>
+</td></tr>
+</table>
