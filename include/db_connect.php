732a0f6aae9180c9f6fad5d8174af13707b7f5e8
Perennate <perennate@gmail.com>
2012-06-24 05:35:14 +0000

    Update install.sql to create _if not exists_, and also made install script do a bit more.

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,4 +1,4 @@
 <?php
-mysql_connect($config['db_hostname'], $config['db_username'], $config['db_password']) or die(mysql_error());
-mysql_select_db($config['db_name']) or die(mysql_error());
+mysql_connect($config['db_hostname'], $config['db_username'], $config['db_password']) or die("Could not connect to MySQL database. Check config.php.<br />" . mysql_error());
+mysql_select_db($config['db_name']) or die("Could not select the oneapp MySQL database. Check config.php.<br />" . mysql_error());
 ?>
