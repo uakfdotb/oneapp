830a633338645b3ea0a75d7313ca7e6eb8307914
vaibhavtheory@gmail.com <vaibhavtheory@gmail.com@ae1f2370-0952-7bf8-395b-7c25bdf261b0>
2012-07-01 22:55:08 +0000

    fixed minor errors, need to create questions that the user cant edit

--- trunk/astyle/two/admin/purchase.php
+++ trunk/astyle/two/admin/purchase.php
@@ -2,23 +2,32 @@
 
 <p>Current Balance: $<?= $balance ?></p>
 
+<form action="purchase.php">
 <table class="tbl_repeat">
 	<tr>
 		<th>Description</th>
 		<th>Date</th>
 		<th>Pending</th>
 		<th>Authorized</th>
+		<th></th>
 	</tr>
 <? while($row = mysql_fetch_array($purchaseHistory)) { ?>
 	<tr>
 		<td><?= $row['description']?></td>
-		<td><?= date('m/d/y H:i:s', $open_time) ?></td>
-		<? if($row['status'] == -1) { ?>
+		<td><?= date('m/d/y H:i:s', $row['submit_time']) ?></td>
+		<? if($row['status'] <= 0) { ?>
 			<td><?= $row['amount'] ?></td>
 			<td></td>
+			<td><button value="<?=$row['id']?>" name="edit">Edit</button></td>
 		<? } else { ?>
 			<td></td>
 			<td><?= $row['amount'] ?></td>
+			<td></td>
 		<? } ?>
 <? } ?>
 </table>
+</form>
+<form action="purchase.php">
+<input name="option" value="true" type="hidden"></input>
+<button>Create New Order</button>
+</form>
