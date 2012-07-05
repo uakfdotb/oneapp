11f4ea4731eb3da7317e14d3b038bc4f219e5c0c
perennate@gmail.com <perennate@gmail.com@ae1f2370-0952-7bf8-395b-7c25bdf261b0>
2012-07-05 06:16:27 +0000

    Fixed a few huge problems with the purchase order tool. Many more huge problems remain...

---
+++ trunk/page/root/man_funds.php
@@ -0,0 +1,94 @@
+<h1>Funding manager</h1>
+<h2>Purchase Orders</h2>
+<h3>Pending</h3>
+
+<form action="man_funds.php">
+<table>
+<tr>
+	<th>Club Name</th>
+	<th>Submit Time</th>
+	<th>Current Status</th>
+	<th>Amount</th>
+	<th></th>
+	<th></th>
+	<th></th>
+</tr>
+
+<? while($row = mysql_fetch_array($pending)) { ?>
+	<tr>
+		<td><? $club_detail = clubInfo($row['club_id']); echo $club_detail[0]; ?></td>
+		<td><?= date('m/d/y H:i', $row['submit_time'])?></td>
+		<td>Awaiting: <?= getPurchaseStatusString($row['status'])?></td>
+		<td><?= $row['amount']?></td>
+		<td><a href="../submit/<?= $row['filename']?>.pdf">Download</a></td>
+		<td><button name="status_change" value="<?=$row['id']?>">Approve</button></td>
+		<td><button name="status_change" value="<?=-1*$row['id']?>">Reject</button></td>
+	</tr>
+<? } ?>
+
+</table>
+</form>
+
+<h3>Completed</h3>
+<table>
+<tr>
+	<th>Club Name</th>
+	<th>Submit Time</th>
+	<th>Finalized Time</th>
+	<th>Status</th>
+	<th>Amount</th>
+</tr>
+
+<? while($row = mysql_fetch_array($completed)) { ?>
+	<tr>
+		<td><? $club_detail = clubInfo($row['club_id']); echo $club_detail[0]; ?></td>
+		<td><?= date('m/d/y H:i', $row['submit_time']) ?></td>
+		<td><? if($row['status'] <= -1) echo date('m/d/y H:i:s', $row['status_time']); ?></td>
+		<td><?= getPurchaseStatusString($row['status'])?></td>
+		<td><?= $row['amount']?></td>
+	</tr>
+<? } ?>
+
+</table>
+
+<form action="man_funds.php?action=add" method="post">
+<table>
+<tr>
+	<td colspan="2">Increase Order</td>
+</tr><tr>
+	<td><input type="text" name="name" list="datalist1"></td>
+	<datalist id="datalist1">
+	<? foreach($userList as $user) {
+				echo "<option value=\"" . $user[0] . "\">";
+	} ?>
+	</datalist>
+	<td><input type="submit" value="Add order" class="add"></td>
+</tr>
+</table>
+</form>
+
+<table class="tbl_repeat">
+<tr>
+	<th align="left">Category</th>
+	<th></th>
+	<th></th>
+	<th></th>
+	<th></th>
+</tr>
+
+<?
+foreach($purchaseList as $item) {
+?>
+	<form method="post" action="man_funds.php">
+	<input type="hidden" name="id" value="<?= $item[0] ?>">
+	<tr align="center"><td><input type="text" name="name" value="<?= $item[1] ?>" style="width:100%"></td>
+	<td><input type="submit" name="action" value="Update" class="update" /></td>
+	<td><input type="submit" name="action" value="Delete" class="delete negative" /></td>
+	<td><input type="submit" name="action" value="up" class="up" /></td>
+	<td><input type="submit" name="action" value="down" class="down" /></td>
+	</tr></form>
+<?
+}
+?>
+
+</table>
